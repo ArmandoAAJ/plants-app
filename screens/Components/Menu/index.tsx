7e4cf16300aefa333d19e142e1bf565d6f004695
Armando Joergensen <armando-a-j@hotmail.com>
2021-08-28 16:16:07 -0300

    Menu Component
    Criando

---
+++ screens/Components/Menu/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import { Container, Title, SubTitle, List, Item, Text } from "./styles";
+
+export const Menu = () => {
+  const [chengeOptin, setChangeOption] = useState("indor");
+
+  return (
+    <Container>
+      <Title>Green</Title>
+      <SubTitle>Plants</SubTitle>
+      <List>
+        <Item
+          onPress={() => setChangeOption("indor")}
+          disabled={chengeOptin === "indor"}
+        >
+          <Text color={chengeOptin === "indor"}>Indor</Text>
+        </Item>
+        <Item
+          onPress={() => setChangeOption("outdor")}
+          disabled={chengeOptin === "outdor"}
+        >
+          <Text color={chengeOptin === "outdor"}>Outdor</Text>
+        </Item>
+        <Item
+          onPress={() => setChangeOption("garden")}
+          disabled={chengeOptin === "garden"}
+        >
+          <Text color={chengeOptin === "garden"}>Garden</Text>
+        </Item>
+      </List>
+    </Container>
+  );
+};
---
+++ screens/Components/Menu/styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components/native";
+
+import Theme from "../../../theme.json";
+
+export const Container = styled.View`
+  position: absolute;
+  background-color: ${Theme.pallet.secondary.color};
+  width: 30%;
+  top: 0;
+  bottom: 0;
+  padding-top: 100px;
+`;
+
+export const Title = styled.Text`
+  display: flex;
+  padding-left: 40%;
+  width: 100%;
+  position: relative;
+  font-size: ${Theme.text.normal}px;
+  color: ${Theme.pallet.secondary.text_color};
+  font-weight: bold;
+`;
+
+export const SubTitle = styled.Text`
+  display: flex;
+  width: 150%;
+  padding-left: 40%;
+  position: relative;
+  font-size: 35px;
+  color: ${Theme.pallet.primary.text_color};
+  font-weight: bold;
+`;
+
+export const List = styled.View`
+  width: 100%;
+`;
+
+export const Item = styled.TouchableOpacity`
+  margin-top: 100px;
+`;
+
+interface PropsText {
+  color: boolean;
+}
+
+export const Text = styled.Text<PropsText>`
+  color: ${({ color }) =>
+    color
+      ? Theme.pallet.primary.text_color
+      : Theme.pallet.secondary.text_color};
+  font-size: ${Theme.text.large}px;
+  font-weight: bold;
+  text-align: center;
+  transform: rotate(270deg);
+`;
