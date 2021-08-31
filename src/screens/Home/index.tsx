e4652135e511a11e4ebe15c7e93b185dac8370c8
Armando Joergensen <armando-a-j@hotmail.com>
2021-08-30 23:11:50 -0300

    Update Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,21 @@
 import React, {useCallback, useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import {FlatList, Text} from 'react-native';
+
+import {useSelector, useDispatch, RootStateOrAny} from 'react-redux';
 
 import {Header} from '../../Components/Header';
 import {Menu} from '../../Components/Menu';
 
 import creator from '../../store/ducks/listPlants';
 
-import {Container} from './styles';
+import {Container, List} from './styles';
 
 export const Home = () => {
     const dispatch = useDispatch();
+    const [isLoading, list] = useSelector((state: RootStateOrAny) => [
+        state.plants.isLoading,
+        state.plants.list,
+    ]);
 
     const load = useCallback(() => {
         dispatch(creator.getPlants());
@@ -17,10 +23,19 @@ export const Home = () => {
 
     useEffect(() => load(), []);
 
+    const renderItem = ({item}) => <Text key={item.id}>{item.name}</Text>;
+
     return (
         <Container>
             <Header bag search />
             <Menu />
+            <List>
+                <FlatList
+                    data={list}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => String(item.id)}
+                />
+            </List>
         </Container>
     );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,7 +1,16 @@
-import styled from "styled-components/native";
-import theme from "../../../theme.json";
+import styled from 'styled-components/native';
+import theme from '../../../theme.json';
 
 export const Container = styled.View`
-  flex: 1;
-  background-color: ${theme.pallet.primary.color};
+    flex: 1;
+    background-color: ${theme.pallet.primary.color};
+`;
+
+export const List = styled.View`
+    position: absolute;
+    left: 30%;
+    top: 120px;
+    bottom: 0;
+    right: 0;
+    background-color: aqua;
 `;
