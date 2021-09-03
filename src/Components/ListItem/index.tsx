bdab64cc0f577daa77eb5677b575336c1f87032f
Armando Joergensen <armando-a-j@hotmail.com>
2021-09-02 23:29:48 -0300

    Update ListItem

--- src/Components/ListItem/index.tsx
+++ src/Components/ListItem/index.tsx
@@ -2,7 +2,34 @@ import React from 'react';
 
 import {Container, Card, Name, AddToCart, SVG, Icon} from './styles';
 
-interface ListProps {
+import {StackNavigationProp} from '@react-navigation/stack';
+
+type RootStackParamList = {
+    Plant: {
+        plant: {
+            id: Number;
+            name: String;
+            abount: String;
+            water_tips: String;
+            photo: String;
+            environments: [String];
+            frequency: {
+                watering: Number;
+                repeat_every: String;
+                height: String;
+                temperature: Number;
+            };
+        };
+    };
+};
+
+type ProfileScreenNavigationProp = StackNavigationProp<
+    RootStackParamList,
+    'Plant'
+>;
+
+type Props = {
+    navigation: ProfileScreenNavigationProp;
     plant: {
         id: Number;
         name: String;
@@ -17,12 +44,12 @@ interface ListProps {
             temperature: Number;
         };
     };
-}
+};
 
-export const ListItem = ({plant}: ListProps) => {
+export const ListItem = ({plant, navigation}: Props) => {
     return (
         <Container>
-            <Card>
+            <Card onPress={() => navigation.navigate('Plant', {plant})}>
                 <SVG uri={plant.photo} />
                 <Name>{plant.name}</Name>
             </Card>
