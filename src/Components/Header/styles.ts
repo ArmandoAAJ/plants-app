4ade2a96b425d354cebbb230b202b845a42f3347
Armando Joergensen <armando-a-j@hotmail.com>
2021-09-04 22:55:35 -0300

    Update Header

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -3,14 +3,14 @@ import { MaterialIcons } from "@expo/vector-icons";
 
 import Theme from "../../../theme.json";
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.View`
   height: 100px;
   background-color: ${Theme.pallet.primary.color};
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  margin: 0 30px;
+  margin: 20px 30px;
 `;
 
 export const Right = styled.View`
