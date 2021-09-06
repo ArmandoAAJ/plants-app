b8e57f270e9b17ebb383eaa74f8acdae19ad79eb
Armando Joergensen <armando-a-j@hotmail.com>
2021-09-05 22:15:06 -0300

    Update Header
    add function go back page home

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,38 +1,41 @@
-import React from "react";
-
-import { Container, Right, Icon, Button } from "./styles";
+import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {Container, Right, Icon, Button} from './styles';
 
 interface PropsHeader {
-  back?: boolean;
-  search?: boolean;
-  bag?: boolean;
+    back?: boolean;
+    search?: boolean;
+    bag?: boolean;
 }
 
-export const Header = ({ back, search, bag }: PropsHeader) => {
-  const handleBack = () => {};
+export const Header = ({back, search, bag}: PropsHeader) => {
+    const navigation = useNavigation();
+    const handleBack = () => {
+        navigation.navigate('Home');
+    };
 
-  const handleSearch = () => {};
+    const handleSearch = () => {};
 
-  const handleBag = () => {};
+    const handleBag = () => {};
 
-  return (
-    <Container>
-      <Button onPress={() => handleBack()}>
-        {back && <Icon name="keyboard-backspace" />}
-      </Button>
+    return (
+        <Container>
+            <Button onPress={() => handleBack()}>
+                {back && <Icon name="keyboard-backspace" />}
+            </Button>
 
-      <Right>
-        {search && (
-          <Button onPress={() => handleSearch()}>
-            <Icon name="search" padding={20} />
-          </Button>
-        )}
-        {bag && (
-          <Button onPress={() => handleBag()}>
-            <Icon name="shopping-bag" />
-          </Button>
-        )}
-      </Right>
-    </Container>
-  );
+            <Right>
+                {search && (
+                    <Button onPress={() => handleSearch()}>
+                        <Icon name="search" padding={20} />
+                    </Button>
+                )}
+                {bag && (
+                    <Button onPress={() => handleBag()}>
+                        <Icon name="shopping-bag" />
+                    </Button>
+                )}
+            </Right>
+        </Container>
+    );
 };
