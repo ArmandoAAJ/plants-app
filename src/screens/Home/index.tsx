1d1b646094d72fa5eb73d7b563492b5297a9a851
Armando Joergensen <armando-a-j@hotmail.com>
2021-09-05 22:19:03 -0300

    Update Index
    Add loader initial request api

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,18 @@
-import React, {useCallback, useEffect} from 'react';
-import {FlatList, Text} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {FlatList} from 'react-native';
 
 import {useSelector, useDispatch, RootStateOrAny} from 'react-redux';
 
+import {Load} from '../../Components/Load';
 import {Header} from '../../Components/Header';
 import {Menu} from '../../Components/Menu';
-import { ListItem } from '../../Components/ListItem';
+import {ListItem} from './ListItem';
 
 import creator from '../../store/ducks/listPlants';
 
 import {Container, List} from './styles';
 
-export const Home = ({navigation}) => {
-    console.log(navigation)
+export const Home: React.FC = (props) => {
     const dispatch = useDispatch();
     const [isLoading, list, listFiltered] = useSelector(
         (state: RootStateOrAny) => [
@@ -21,9 +21,14 @@ export const Home = ({navigation}) => {
             state.plants.listFiltered,
         ],
     );
+    const [loading, setLoading] = useState(true);
 
     const load = useCallback(() => {
+        if (isLoading) return;
         dispatch(creator.getPlants());
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
     }, []);
 
     useEffect(() => load(), []);
@@ -33,11 +38,14 @@ export const Home = ({navigation}) => {
             <Header bag search />
             <Menu />
             <List>
-                <FlatList
-                    data={listFiltered.length < 1 ? list : listFiltered}
-                    renderItem={({item}) => <ListItem plant={item} navigation={navigation}/>}
-                    keyExtractor={(item) => String(item.id)}
-                />
+                {loading && <Load />}
+                {!loading && (
+                    <FlatList
+                        data={listFiltered.length > 0 ? listFiltered : list}
+                        renderItem={({item}) => <ListItem plant={item} />}
+                        keyExtractor={(item) => String(item.id)}
+                    />
+                )}
             </List>
         </Container>
     );
