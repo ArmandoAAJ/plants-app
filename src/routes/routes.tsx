68fcb86ad27a10a182e53cdc906c0f5fe9d05ebf
Armando Joergensen <armando-a-j@hotmail.com>
2021-09-14 20:16:34 -0300

    Update Routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import {Home} from '../screens/Home';
 import {Plant} from '../screens/Plant';
+import {Cart} from '../screens/Cart';
 
 const stackRoutes = createStackNavigator();
 
@@ -11,6 +12,7 @@ export const Routes: React.FC = () => {
         <stackRoutes.Navigator headerMode="none">
             <stackRoutes.Screen name="Home" component={Home} />
             <stackRoutes.Screen name="Plant" component={Plant} />
+            <stackRoutes.Screen name="Cart" component={Cart} />
         </stackRoutes.Navigator>
     );
 };
