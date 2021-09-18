13d58083d96e1bc701da633f40d4e40c2382c2bc
Armando Joergensen <armando-a-j@hotmail.com>
2021-09-17 23:38:29 -0300

    Update cart

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Header } from '../../Components/Header';
+
+import {Header} from '../../Components/Header';
+import Product from '../../Components/Product';
+
+import {Container} from './styles';
 
 export const Cart: React.FC = () => {
-  return(
-  <View>
-    <Header
-      bag
-      back
-    >
-  </View>
-  )
-}
-
+    return (
+        <Container>
+            <Header back title="My Cart" />
+            <Product/>
+        </Container>
+    );
+};
