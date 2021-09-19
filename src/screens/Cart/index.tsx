7a9607656004d0d49b74e799599dcbd1f5e688ec
Armando Joergensen <armando-a-j@hotmail.com>
2021-09-18 23:33:33 -0300

    Update Cart

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, {useCallback, useEffect} from 'react';
+import {FlatList} from 'react-native';
+import {useSelector, useDispatch, RootStateOrAny} from 'react-redux';
+
+import creator from '../../store/ducks/cart';
 
 import {Header} from '../../Components/Header';
 import Product from '../../Components/Product';
@@ -6,10 +10,37 @@ import Product from '../../Components/Product';
 import {Container} from './styles';
 
 export const Cart: React.FC = () => {
+    const dispatch = useDispatch();
+    const [isLoading, cart] = useSelector((state: RootStateOrAny) => [
+        state.cart.isLoading,
+        state.cart.cart,
+    ]);
+
+    const load = useCallback(() => {
+        if (isLoading) return;
+        dispatch(creator.getCart());
+        handleAddProduct(1);
+    }, []);
+
+    useEffect(() => load(), []);
+
+    const handleAddProduct = (value: number) => {
+        dispatch(creator.addCart(value));
+    };
+
     return (
         <Container>
             <Header back title="My Cart" />
-            <Product/>
+            <FlatList
+                data={cart}
+                renderItem={({item}) => (
+                    <Product
+                        addToCart={(value: number) => handleAddProduct(value)}
+                        item={item}
+                    />
+                )}
+                keyExtractor={(item) => item.id}
+            />
         </Container>
     );
 };
