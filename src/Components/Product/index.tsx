c4d249b118ceed79e0217b0aad88c261b900f0e9
Armando Joergensen <armando-a-j@hotmail.com>
2021-09-18 23:36:08 -0300

    Update Product

--- src/Components/Product/index.tsx
+++ src/Components/Product/index.tsx
@@ -1,5 +1,4 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {useSelector, useDispatch, RootStateOrAny} from 'react-redux';
 import {
     Container,
     SVG,
@@ -9,61 +8,43 @@ import {
     Actions,
     Icon,
 } from './styles';
-import creator from '../../store/ducks/listPlants';
 
-const Product: React.FC = () => {
-    const dispatch = useDispatch();
-    const [isLoading, list, listFiltered] = useSelector(
-        (state: RootStateOrAny) => [
-            state.plants.isLoading,
-            state.plants.list,
-            state.plants.listFiltered,
-        ],
-    );
-
-    const load = useCallback(() => {
-        if (isLoading) return;
-        dispatch(creator.getPlants());
-    }, []);
-
-    useEffect(() => load(), []);
+const Product: React.FC = ({item, addToCart, removeToCart}) => {
     return (
-        <>
-            {list.map((p) => (
-                <Container>
-                    <SVG uri={p.photo} />
-                    <Content>
-                        <Name>{p.name}</Name>
-                        <CardButtons>
-                            <Actions
-                                style={{
-                                    borderTopLeftRadius: 5,
-                                    borderBottomLeftRadius: 5,
-                                }}>
-                                <Icon name="add" />
-                            </Actions>
-                            <Actions
-                                disabled
-                                style={{
-                                    borderRightWidth: 0.5,
-                                    borderRightColor: '#000',
-                                    borderLeftColor: '#000',
-                                    borderLeftWidth: 0.2,
-                                }}>
-                                <Name>5</Name>
-                            </Actions>
-                            <Actions
-                                style={{
-                                    borderTopRightRadius: 5,
-                                    borderBottomRightRadius: 5,
-                                }}>
-                                <Icon name="remove" />
-                            </Actions>
-                        </CardButtons>
-                    </Content>
-                </Container>
-            ))}
-        </>
+        <Container>
+            <SVG uri={item.photo} />
+            <Content>
+                <Name>{item.name}</Name>
+                <CardButtons>
+                    <Actions
+                        onPress={() => addToCart(5)}
+                        style={{
+                            borderTopLeftRadius: 5,
+                            borderBottomLeftRadius: 5,
+                        }}>
+                        <Icon name="add" />
+                    </Actions>
+                    <Actions
+                        disabled
+                        style={{
+                            borderRightWidth: 0.5,
+                            borderRightColor: '#000',
+                            borderLeftColor: '#000',
+                            borderLeftWidth: 0.2,
+                        }}>
+                        <Name>5</Name>
+                    </Actions>
+                    <Actions
+                        onPress={removeToCart}
+                        style={{
+                            borderTopRightRadius: 5,
+                            borderBottomRightRadius: 5,
+                        }}>
+                        <Icon name="remove" />
+                    </Actions>
+                </CardButtons>
+            </Content>
+        </Container>
     );
 };
 
