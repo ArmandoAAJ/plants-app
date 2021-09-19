a476acc00d7d34e1fee657c832759cf9f69f4a11
Armando Joergensen <armando-a-j@hotmail.com>
2021-09-18 23:33:54 -0300

    Update Saga Duck Root

--- src/store/ducks/index.ts
+++ src/store/ducks/index.ts
@@ -1,8 +1,10 @@
-import { combineReducers } from "redux";
-import { reducer as plants } from "./listPlants";
+import {combineReducers} from 'redux';
+import {reducer as plants} from './listPlants';
+import {reducer as cart} from './cart';
 
 const reducers = combineReducers({
-  plants,
+    plants,
+    cart,
 });
 
 export default reducers;
--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -1,6 +1,7 @@
-import { all } from "redux-saga/effects";
-import plants from "./listPlants";
+import {all} from 'redux-saga/effects';
+import plants from './listPlants';
+import cart from './cart';
 
 export default function* rootSaga() {
-  yield all([plants()]);
+    yield all([plants(), cart()]);
 }
