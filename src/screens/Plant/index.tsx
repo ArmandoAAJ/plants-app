08e68d9ffa71b6382e18128a01b8b9a91237781d
Armando Joergensen <armando-a-j@hotmail.com>
2021-09-21 23:18:56 -0300

    Update Plant

--- src/screens/Plant/index.tsx
+++ src/screens/Plant/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigatorScreenParams} from '@react-navigation/native';
 
-import {Header} from '../../Components/Header';
+import {Header} from '../../components/Header';
 
 import {PlantProps} from '../../config/types';
 
